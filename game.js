ec275ba67f2b8dc62c78b1b43fb73c7bac6152af
Lonnie Jones <lonnie34j@gmail.com>
2016-10-02 02:03:57 -0400

    initial save files

--- game.js
+++ game.js
@@ -1,13 +1,9 @@
-var characters = ["ryu","ken","sagat","chunli","mbison","balrog","ehonda","guile","zangief","cammy","vega","dhalsim","blanka"];
-var userWins = 0;
-var triesRemaining = 6;
-var currentWord;
-var placeholder;
-var correctGuess;
-var losses = 0;
-var userGuesses = [];
+console.log("in working condition");
 
+var myHangwords = require("./words.js")
 
 function randomWord() {
-	currentWord = characters[Math.floor(Math.random()*characters.length)];
+	var currentWord = array[Math.floor(Math.random()*characters.length)];
 	console.log(currentWord);
+
+}
---
+++ word.js
@@ -0,0 +1,10 @@
+
+	function hangWords(ryu, ken, chunli, sagat, dhalsim, zangeif, ehonda, cammy, balrog, mbison, guile, vega, blanka) {
+		this.ryu = ryu;
+		this.ken = ken;
+		this.chunli = chunli;
+		this.sagat = sagat;
+		this.mbison = mbison;
+	}
+
+modules.exports = hangWords;
